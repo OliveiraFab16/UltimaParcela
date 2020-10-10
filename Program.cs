7d8057c00e302230c91d415b39bffa2bac4df122
oliveiraFab16 <fabriciocarvalho932@gamil.com>
2020-10-09 22:09:44 -0300

    UltimaParcela

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Figgle;
+
+namespace UltimaParcela
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal ValorTotal;
+            decimal Parcelas;
+            decimal ValorFinal;
+
+            DateTime dataAtual = DateTime.Now;
+            DateTime ParcelaFinal;
+
+            Console.WriteLine("----Ultima Parcela----!");
+
+            Console.Write(" Valor Total Das Parcelas: ");
+            ValorFinal = decimal.Parse(Console.ReadLine());
+
+            Console.Write("Valor de Cada Parcela: ");
+            Parcelas = decimal.Parse(Console.ReadLine());
+
+            ValorTotal = ValorFinal / Parcelas;
+
+            ParcelaFinal = dataAtual.AddMonths(8);
+            Console.WriteLine($"A Ultima Parcela Será Em: {ParcelaFinal}, No Valor De R${ValorTotal}");
+
+                        
+            Console.WriteLine(FiggleFonts.Standard.Render("OBRIGADO!"));
+
+
+
+
+
+
+
+            
+            
+
+        }
+    }
+}
